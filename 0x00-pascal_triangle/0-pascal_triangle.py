e766462ffb7c51f27e6a65203d39c5a07deb22dd
Aymen Hommani <crosspieloo@gmail.com>
2025-04-18 03:54:24 +0100

    uploading pascal triangle from WSL

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    else:
+        tri_list = []
+        for i in range(n):
+            if i == 0:
+                tri_list.append([1])
+            else:
+                new_list = [1]
+                prev = tri_list[i - 1]
+                for j in range(len(prev) - 1):
+                    sum = prev[j] + prev[j + 1]
+                    new_list.append(sum)
+
+                new_list.append(1)
+                tri_list.append(new_list)
+        return tri_list
