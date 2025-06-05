369055241404720a9801984399c45c2ec59d6a3d
Aymen Hommani <crosspieloo@gmail.com>
2025-06-05 05:07:17 +0100

    uploading the module

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module to Rotate a 2D Matrix """
+
+
+def rotate_2d_matrix(matrix):
+    """ rotates an n x n 2d matrix by 90 """
+    l, r = 0, len(matrix) - 1
+
+    while l < r:
+        for i in range(r - l):
+            top, bottom = l, r
+
+            top_left = matrix[top][l + i]
+
+            matrix[top][l + i] = matrix[bottom - i][l]
+
+            matrix[bottom - i][l] = matrix[bottom][r - i]
+
+            matrix[bottom][r - i] = matrix[top + i][r]
+
+            matrix[top + i][r] = top_left
+        
+        r -= 1
+        l += 1
