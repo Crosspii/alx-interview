d4964db57fd9aa193bb23940df04c69fc802bcc3
Aymen hommani <crosspieloo@gmail.com>
2025-06-13 03:47:02 +0100

    making it optmized

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,9 +4,12 @@
 
 def makeChange(coins, total):
     """Determine the fewest number of coins needed to make a given amount."""
+    if total <= 0:
+        return 0
+    if not coins:
+        return -1
     dp = [total + 1] * (total + 1)
-    dp[0] = 0
-
+    
     for amount in range(1, total + 1):
         for coin in coins:
             if amount - coin >= 0:
